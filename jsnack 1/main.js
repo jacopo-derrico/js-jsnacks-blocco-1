76e4431c12752cc93b6e49275f519f0d90ce9824
jacopo-derrico <derricoj.design@gmail.com>
2024-01-11 16:13:00 +0100

    jsnack 1 complete

--- jsnack 1/main.js
+++ jsnack 1/main.js
@@ -1,6 +1,12 @@
-let insertNumber = prompt("Enter a number")
+const result = document.getElementById('result')
+let sum = 0
 
-for (let i = 0; i <= 10; i++) {
-    const element = array[i];
+for (let i = 0; i < 10; i++) {
+    let insertNumber = parseFloat(prompt("Enter a number"));
+
+    console.log(insertNumber);
+
+    sum += insertNumber;
     
+    result.innerHTML = `The sum is: ${sum}`
 }
